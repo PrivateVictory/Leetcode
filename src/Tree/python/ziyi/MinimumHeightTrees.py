749ee75df4cade8f679cef998e714f4432ec593a
sisiruowan <1174779123@qq.com>
2016-01-06 21:01:50 +0800

    2016/1/5 finish 5 works

---
+++ src/Tree/python/ziyi/MinimumHeightTrees.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2015-12-22 21:37:03
+# @Author  : Alex Tang (1174779123@qq.com)
+# @Link    : http://t1174779123.iteye.com
+
+'''
+	description:
+'''
+
+class Solution(object):
+    def findMinHeightTrees(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: List[int]
+        """
+        
