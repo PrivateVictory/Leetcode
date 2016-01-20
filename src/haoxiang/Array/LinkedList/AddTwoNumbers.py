053ccc1f630dbcc42fd0d5e23d0d18b98b867a84
haoxiang47 <352183483@qq.com>
2016-01-20 20:10:16 +0800

    solve 2 problem

---
+++ src/haoxiang/Array/LinkedList/AddTwoNumbers.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @return a ListNode
+    def addTwoNumbers(self, l1, l2):
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        flag = 0
+        dummy = ListNode(0); p = dummy
+        while l1 and l2:
+            p.next = ListNode((l1.val+l2.val+flag) % 10)
+            flag = (l1.val+l2.val+flag) / 10
+            l1,l2,p = l1.next,l2.next,p.next
+        if l2:
+            while l2:
+                p.next = ListNode((l2.val+flag) % 10)
+                flag = (l2.val+flag) / 10
+                l2,p = l2.next,p.next
+        if l1:
+            while l1:
+                p.next = ListNode((l1.val+flag) % 10)
+                flag = (l1.val+flag) / 10
+                l1,p = l1.next, p.next
+        if flag == 1: p.next = ListNode(1)
+        return dummy.next
