373d494cfc482b3ffba4e158dde7139a4b40a05c
Kang-Li (Stephen) Cheng <kangli.cheng@gmail.com>
2017-05-18 01:07:06 -0400

    Create isAnagram.py

---
+++ src/klc/hashtable/isAnagram.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        we sort both lists and compare them, returns true if they are anagrams of each other
+        overall complexity is O(n lg n), from calling sorted() in python
+        """
+        return sorted(t)==sorted(s) 
+ 
+
+    def isAnagramFast(self, s, t):
+     #if both lists are equal length, check if character counts are equal
+     #fast solution that is O(n)
+        if len(s)==len(t):
+            for char in set(s):
+                if s.count(char)!=t.count(char):
+                    return False
+            return True
+        else: #clearly not anagrams if s and t have different lengths
+            return False
+            
+    def isAnagram2(self, s, t):
+     #create dictionaries for s and t
+        dic1, dic2 = {}, {}
+        for item in s:
+            dic1[item] = dic1.get(item, 0) + 1
+        for item in t:
+            dic2[item] = dic2.get(item, 0) + 1
+        return dic1 == dic2
