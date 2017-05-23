ad48a6f6330627c0c0feb472c3861ec5760982c2
Kang-Li (Stephen) Cheng <kangli.cheng@gmail.com>
2017-05-23 15:19:53 -0400

    Update isAnagram.py

--- src/klc/hashtable/isAnagram.py
+++ src/klc/hashtable/isAnagram.py
@@ -29,3 +29,11 @@ class Solution(object):
         for item in t:
             dic2[item] = dic2.get(item, 0) + 1
         return dic1 == dic2
+    
+    def isAnagram2(self, s, t):
+        dic1, dic2 = [0]*26, [0]*26
+        for item in s:
+            dic1[ord(item)-ord('a')] += 1
+        for item in t:
+            dic2[ord(item)-ord('a')] += 1
+        return dic1 == dic2
